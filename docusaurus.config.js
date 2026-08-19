eb46fcd8b2185c9704078a02dcbd87e2b399873a
aminreza_sefid <aminreza_sefid@yahoo.com>
2026-08-19 13:46:46 +0330

    asdasd

---
+++ docusaurus.config.js
@@ -0,0 +1,13 @@
+export default {
+  // ...
+
+  url: 'https://aminrezasefid.github.io',
+  baseUrl: '/my-docs/',
+
+  organizationName: 'aminrezasefid',
+  projectName: 'my-docs',
+
+  trailingSlash: false,
+
+  // ...
+};
